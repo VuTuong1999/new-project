a997926e315a875478f85633e83894a54dd406b8
VuTuong1999 <vungoctuong2@gmail.com>
2022-02-19 00:37:50 +0700

    sửa get started

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import demo from './components/demo'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
